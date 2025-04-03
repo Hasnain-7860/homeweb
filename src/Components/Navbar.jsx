5e7ebdb4c095605b980aac1dd7c437fea69b9c76
Hasnain-7860 <154454618+Hasnain-7860@users.noreply.github.com>
2025-04-03 23:59:24 +0530

    slider all

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,10 +8,10 @@ const Navbar = () => {
         <div className='container mx-auto flex  justify-between items-center py-4 px-6 md:px-20 lg:px-32'>
             <img className='' src={assets.logo} alt="" />
             <ul className='hidden md:flex gap-7 text-white'>
-                <li className='cursor-pointer hover:text-gray-400'>Home</li>
+                <li className='cursor-pointer hover:text-gray-400'><a href="#Home"> Home </a></li>
                 <li  className='cursor-pointer hover:text-gray-400'><a href="#About"> About</a></li>
-                <li className='cursor-pointer hover:text-gray-400'>Projects</li>
-                <li className='cursor-pointer hover:text-gray-400'>Testimonials</li>
+                <li className='cursor-pointer hover:text-gray-400'> <a href="#Projects">Projects</a></li>
+                <li className='cursor-pointer hover:text-gray-400'><a href="#Testimonials">Testimonials</a></li>
             </ul>
             <button className='hidden md:block bg-white py-2  px-8 rounded-full '>Sign up</button>
             <img onClick={()=>setShowMobileMenu(true)} className='md:hidden w-7 cursor-pointer' src={assets.menu_icon} alt="" />
