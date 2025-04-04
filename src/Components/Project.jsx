8a82639abe4740e79d9dbf2250674dac3f8ef369
Hasnain-7860 <154454618+Hasnain-7860@users.noreply.github.com>
2025-04-04 23:56:21 +0530

    slider

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import leftarrow from '../assets/left_arrow.svg'
 import rightarrow from '../assets/right_arrow.svg'
 import { projectsData } from '../assets/assets'
  
 const Project = () => {
+    const [currentIndex ,setCurrentIndex] = useState(0);
+    const[cardSToShow , setCardsToShow] = useState(1);
+
+    const nextProject =()=>{
+        setCurrentIndex((prevIndex)=>(prevIndex+1) % projectsData.lenght)
+    }
+    const preProject =()=>{
+        setCurrentIndex((prevIndex)=> prevIndex === 0 ? projectsData.length-1: 
+    
+    )
+    }
+
   return (
     <div className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='Project'>
         <h1 className='text-2xl sm:text-4xl font-bold mb-2  text-center'>
