85c79f61e8346939a3fa7c3b6caac25ae3f9c618
Hasnain-7860 <154454618+Hasnain-7860@users.noreply.github.com>
2025-04-05 08:22:41 +0530

    slider done

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,17 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import leftarrow from '../assets/left_arrow.svg'
 import rightarrow from '../assets/right_arrow.svg'
 import { projectsData } from '../assets/assets'
+import { assets } from '../assets/assets'
  
 const Project = () => {
     const [currentIndex ,setCurrentIndex] = useState(0);
-    const[cardSToShow , setCardsToShow] = useState(1);
+    const[cardsToShow , setCardsToShow] = useState(1);
+
+    useEffect(()=>{
+        const updateCardsToShow =()=>{
+            if(window.innerWidth >=1024){
+                setCardsToShow(projectsData.length);
+            }
+            else{
+                setCardsToShow(1)
+            };
+        };
+            updateCardsToShow();
+            window.addEventListener('resize',updateCardsToShow);
+            return()=>window.removeEventListener('resize',updateCardsToShow)
+        
+    },[])
 
     const nextProject =()=>{
-        setCurrentIndex((prevIndex)=>(prevIndex+1) % projectsData.lenght)
+        setCurrentIndex((prevIndex)=>(prevIndex + 1) % projectsData.length)
     }
-    const preProject =()=>{
-        setCurrentIndex((prevIndex)=> prevIndex === 0 ? projectsData.length-1: 
+    const prevProject =()=>{
+        setCurrentIndex((prevIndex)=> prevIndex === 0 ? projectsData.length -1 : prevIndex -1
     
     )
     }
@@ -22,15 +38,19 @@ const Project = () => {
             Projects <span className='underline underline-offset-4 decoration-1 under font-light'>Completed</span> </h1>
             <p className='text-center text-gray-500 mb-8  mx-auto max-w-80 '>Crafting Spaces , Building Legacies-Explore our Portfolio</p>
             <div className='flex justify-end items-center mb-2'>
-                <button className='p-3 bg-gray-200 rounded mr-2'>
+                <button onClick={prevProject}
+                 className='p-3 bg-gray-200 rounded mr-2'>
                 <img src={leftarrow} alt="" />
                 </button>
-                <button className='p-3 bg-gray-200 rounded mr-2'>
+                <button onClick={nextProject}
+                 className='p-3 bg-gray-200 rounded mr-2'>
                 <img src={rightarrow} alt="" />
                 </button>
             </div>
             <div className='overflow-hidden'>
-                <div className='flex gap-8  transition-transform duration-500 ease-in-out'>
+                <div className='flex gap-8  transition-transform duration-500 ease-in-out'
+                style={{transform:`translateX(-${(currentIndex * 100) / cardsToShow}%)`}}
+                >
                     {projectsData.map((project,index)=>(
                         <div key={index} className='  w-1/1 lg:relative flex-shrink-0 lg:w-1/4  '>
                             <img className='w-full h-auto mb-14' src={project.image} alt={project.title} />
